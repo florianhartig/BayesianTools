34fb776b530ef04da497637512447b5b55f14f6d
stefan-paul <stefan_paul_1@web.de>
2017-02-04 16:51:10 +0100

    Test update

--- BayesianTools/tests/testthat/test-convergence.R
+++ BayesianTools/tests/testthat/test-convergence.R
@@ -1,58 +1,66 @@
-# context("Test convergence of all samplers")
-# 
-# skip_on_cran()
-# 
-# 
-# set.seed(1)
-# library(BayesianTools)
-# 
-# 
-# testfun <- list(testDensityNormal, testDensityMultiNormal, testDensityBanana)
-# nrPar <- c(1,3,2)
-# 
-# samp <- getPossibleSamplerTypes()
-# 
-# 
-# reference <- list(rnorm(1000), 
-#                   mvtnorm::rmvnorm(1000, mean = rep(0, 3), sigma =  emulator::corr.matrix(cbind(c(0.2, 0.3, 0.3503)),scales=1) ),
-#                   mvtnorm::rmvnorm(1000, mean = rep(0, 2), sigma = matrix(nrow = 2, data = c(1, 0.9, 0.9, 1))) )
-# 
-# 
-# iter = 1000000
-# settings <- list(iterations = iter, message = FALSE)
-# 
-# for(i in 1:length(testfun)){
-#   
-#   fun <- testfun[[i]]
-#   setup <- createBayesianSetup(likelihood = fun, lower = rep(-10, nrPar[i]), 
-#                                upper = rep(10, nrPar[i]))
-#   
-#   for(k in 1:(length(samp$BTname)-1)){
-#     if(nrPar[i] != 1 | samp$univariatePossible[k] == T){ 
-#     out <- runMCMC(setup, sampler = samp$BTname[k], settings = settings)
-#     
-#       
-#     sample <- getSample(out, numSamples = 1000, start = 1000)
-#     
-#     if(nrPar[i] == 1){
-#       ks <- ks.test(as.vector(sample), reference[[i]])$p.value
-#       expect_true(ks>0.05)
-#       
-#     }else{
-#     for(z in 1:ncol(sample)){
-#      ks <- ks.test(sample[,z], reference[[i]][,z])$p.value
-#      expect_true(ks>0.01) 
-#     }
-#     }
-#     
-#     #ksVal <-  ks.boot(getSample(out, numSamples = 10000), rnorm(10000))$ks.boot.pvalue 
-#     } # if
-#     
-#     }
-# 
-#   }
-#   
-#  
-#   
-# 
-# 
+context("Test convergence of all samplers")
+
+
+
+set.seed(1)
+library(BayesianTools)
+
+
+testfun <- testDensityNormal # TODO replace 1d normal 
+nrPar <- c(1,2)
+
+samp <- c(rep("Metropolis", 4), "DE", "DEzs", "DREAM", "DREAMzs", "Twalk")
+
+
+
+iter = 100000
+
+settings <- list(list(iterations = iter, adapt = F, message = FALSE),
+                 list(iterations = iter, adapt = T, message = FALSE),
+                 list(iterations = iter, adapt = F, DRlevels = 2, message = FALSE),
+                 list(iterations = iter, adapt = T, DRlevels = 2, message = FALSE),
+                 list(iterations = iter, message = FALSE),
+                 list(iterations = iter, message = FALSE),
+                 list(iterations = iter, message = FALSE),
+                 list(iterations = iter, message = FALSE),
+                 list(iterations = iter, message = FALSE)
+                 
+                 )
+
+
+test_that("All samplers sample correctly",{
+      skip_on_cran()
+      skip_on_travis()
+          
+for(i in 1:2){
+
+  fun <- testfun
+  setup <- createBayesianSetup(likelihood = fun, lower = rep(-10, nrPar[i]),
+                               upper = rep(10, nrPar[i]))
+
+  for(k in 1:(length(samp))){
+    out <- runMCMC(setup, sampler = samp[k], settings = settings[[k]])
+
+    sample <- getSample(out, numSamples = 1000, start = 1000)
+
+    if(nrPar[i] == 1){
+      ks <- suppressWarnings(ks.test(as.vector(sample), rnorm(1000))$p.value)
+      expect_true(ks>0.01)
+
+    }else{
+    for(z in 1:ncol(sample)){
+     ks <- suppressWarnings(ks.test(sample[,z], rnorm(1000))$p.value)
+     expect_true(ks>0.01)
+    }
+    }
+
+    #ksVal <-  ks.boot(getSample(out, numSamples = 10000), rnorm(10000))$ks.boot.pvalue
+
+    }
+
+  }}
+)
+
+
+
+
