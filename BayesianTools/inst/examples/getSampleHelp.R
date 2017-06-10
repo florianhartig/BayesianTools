956c3cc7c751cdc68e0512ec4c5867fea81baaf7
TankredO <Tankred_ott@hotmail.de>
2017-06-10 15:07:10 +0200

    added example with single mcmc chain to getSampleHelp
    Also updated documentation

--- BayesianTools/inst/examples/getSampleHelp.R
+++ BayesianTools/inst/examples/getSampleHelp.R
@@ -5,11 +5,18 @@ setup = createBayesianSetup(ll, lower = c(-10,-10), upper = c(10,10))
 settings = list(nrChains = 2, iterations = 1000)
 out <- runMCMC(bayesianSetup = setup, sampler = "DEzs", settings = settings)
 
-
 # population MCMCs divide the interations by the number of internal chains,
 # so the end of the 3 chains is 1000/3 = 334
 sample <- getSample(out, start = 100, end = 334, thin = 10) 
 
-# 
+# sampling with number of samples instead of thinning and
+# returning a coda object
 sample <- getSample(out, start = 100, numSamples = 60, coda = TRUE)
 plot(sample)
+
+
+# MCMC with a single chain:
+settings_2 <- list(nrChains = 1, iterations = 1000)
+out_2 <- runMCMC(setup, sampler = "Metropolis", settings = settings_2)
+sample_2 <- getSample(out_2, numSamples = 100)
+
