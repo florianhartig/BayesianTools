c070a5235f592a6601ecb2babab4e7106fafd25f
PC1011705265\LocalAdmin <Tankred_ott@hotmail.de>
2018-03-20 14:51:52 +0100

    preOptimize with mvnorm

--- BayesianTools/R/preOptimize.R
+++ BayesianTools/R/preOptimize.R
@@ -1,9 +1,11 @@
-/dontrun {
-
 p = createPrior(function(x) sum(dnorm(x, 0, 50, TRUE)), function(n=1) rnorm(n, 0, 50))
 ll = function(x) sum(dnorm(x, 3, 2, TRUE))
 bs = createBayesianSetup(ll, p)
 
+p = createPrior(function(x) sum(mvtnorm::dmvnorm(x, c(0,0,0), diag(3) * c(100, 100, 100)), TRUE),
+                function(n=1) mvtnorm::rmvnorm(n, c(0,0,0), diag(3) * c(100, 100, 100)))
+ll = function(x) sum(mvtnorm::dmvnorm(x, c(4, 2, 7), diag(3) * c(4, 2, 15)))
+bs = createBayesianSetup(ll, p)
 
 preOptimize = function(bayesianSetup) {
   target <- function(x) {
@@ -24,54 +26,54 @@ preOptimize = function(bayesianSetup) {
   optresul
 }
 
-preOptimize(bs)
+preOpt = preOptimize(bs)
+# negative inverse of the hessian is (asymptotic) covariance matrix
+Matrix::nearPD(-MASS::ginv(numDeriv::hessian(target, preOpt$par)))$mat
 
 
 
-
-if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
-else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)      
-
-
-
-if(is.null(settings$message) || settings$message == TRUE){
-  cat("BT runMCMC: trying to find optimal start and covariance values", "\b")
-}
-
-target <- function(x){
-  out <- bayesianSetup$posterior$density(x)
-  if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
-  return(out)
-}
-
-try( {
-  if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
-  else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)      
-  settings$startValue = optresul$par
-  hessian = numDeriv::hessian(target, optresul$par)
-  
-  
-  proposalGenerator$covariance = as.matrix(Matrix::nearPD(MASS::ginv(-hessian))$mat)
-  #proposalGenerator$covariance = MASS::ginv(-optresul$hessian)
-  
-  # Create objects for startValues and covariance to add space between values
-  startV <-covV <- character()
-  
-  for(i in 1:length(settings$startValue)){
-    startV[i] <- paste(settings$startValue[i], "")
-  } 
-  for(i in 1:length( proposalGenerator$covariance)){
-    covV[i] <- paste( proposalGenerator$covariance[i], "")
-  } 
-  
-  if(is.null(settings$message) || settings$message == TRUE){
-    message("BT runMCMC: Optimization finished, setting startValues to " , 
-            startV, " - Setting covariance to " , covV)
-  }
-  
-  proposalGenerator = updateProposalGenerator(proposalGenerator)
-  
-}
-, silent = FALSE)
-
-}
+# if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
+# else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)
+# 
+# 
+# 
+# if(is.null(settings$message) || settings$message == TRUE){
+#   cat("BT runMCMC: trying to find optimal start and covariance values", "\b")
+# }
+# 
+# target <- function(x){
+#   out <- bayesianSetup$posterior$density(x)
+#   if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
+#   return(out)
+# }
+# 
+# try( {
+#   if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
+#   else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)
+#   settings$startValue = optresul$par
+#   hessian = numDeriv::hessian(target, optresul$par)
+# 
+# 
+#   proposalGenerator$covariance = as.matrix(Matrix::nearPD(MASS::ginv(-hessian))$mat)
+#   #proposalGenerator$covariance = MASS::ginv(-optresul$hessian)
+# 
+#   # Create objects for startValues and covariance to add space between values
+#   startV <-covV <- character()
+# 
+#   for(i in 1:length(settings$startValue)){
+#     startV[i] <- paste(settings$startValue[i], "")
+#   }
+#   for(i in 1:length( proposalGenerator$covariance)){
+#     covV[i] <- paste( proposalGenerator$covariance[i], "")
+#   }
+# 
+#   if(is.null(settings$message) || settings$message == TRUE){
+#     message("BT runMCMC: Optimization finished, setting startValues to " ,
+#             startV, " - Setting covariance to " , covV)
+#   }
+# 
+#   proposalGenerator = updateProposalGenerator(proposalGenerator)
+# 
+# }
+# , silent = FALSE)
+# 
