6b9fb6061f721193fbd2a8f746a0c3d9b4f9fe45
PC1011705265\LocalAdmin <Tankred_ott@hotmail.de>
2018-03-29 11:09:49 +0200

    preOptimizeMetropolis

--- BayesianTools/R/preOptimize.R
+++ BayesianTools/R/preOptimize.R
@@ -7,6 +7,12 @@ p = createPrior(function(x) sum(mvtnorm::dmvnorm(x, c(0,0,0), diag(3) * c(100, 1
 ll = function(x) sum(mvtnorm::dmvnorm(x, c(4, 2, 7), diag(3) * c(5, 2, 15), TRUE))
 bs = createBayesianSetup(ll, p)
 
+target <- function(x) {
+  out <- bs$posterior$density(x)
+  if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
+  return(out)
+}
+
 preOptimize = function(bayesianSetup) {
   target <- function(x) {
     out <- bayesianSetup$posterior$density(x)
@@ -19,14 +25,17 @@ preOptimize = function(bayesianSetup) {
   
   lower = if (!is.null(bayesianSetup$prior$lower)) bayesianSetup$prior$lower else rep(-1e20, nPar)
   upper = if (!is.null(bayesianSetup$prior$upper)) bayesianSetup$prior$upper else rep(+1e20, nPar)
-  startValue = if (!is.null(settings$startValue)) settings$startValue else (lower + upper) / 2
+  # startValue = if (!is.null(settings$startValue)) settings$startValue else (lower + upper) / 2
+  startVal = bayesianSetup$prior$sampler()
+  print(startVal)
 
-  if(nPar > 1) optresul <- optim(par=startValue, fn = target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
-  else optresul <- optim(par=startValue, fn = target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = lower, upper = upper)      
+  if(nPar > 1) optresul <- optim(par=startVal, fn = target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
+  else optresul <- optim(par=startVal, fn = target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = lower, upper = upper)      
   optresul
 }
 
 preOpt = preOptimize(bs)
+(newStartval = preOpt$par)
 
 # Metropolis needs one startValue
 # and a covariance matrix
@@ -43,55 +52,56 @@ t(sapply(startVals, function(optResult) optResult$par, simplify = "matrix"))
 
 # DEzs needs npar * 3 start values
 # and a npar * 10 z matrix
+#   draw from a mvnormal distribution around the start values?
+#   randomize around startvalues?
+#   do multiple optimizations?
+
+# DREAM needs max(4, npar * 2) start values
+
+# DREAMzs needs npar * 3 start values
+# and a npar * 10 z matrix
+
+
+function preOptimize(bayesianSetup, sampler) {
+  target <- function(x) {
+    out <- bayesianSetup$posterior$density(x)
+    if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
+    return(out)
+  }
+
+  settings = bayesianSetup$settings
+  nPar = bayesianSetup$numPars
+
+  lower = if (!is.null(bayesianSetup$prior$lower)) bayesianSetup$prior$lower else rep(-1e20, nPar)
+  upper = if (!is.null(bayesianSetup$prior$upper)) bayesianSetup$prior$upper else rep(+1e20, nPar)
+  startVal = if (!is.null(settings$startValue)) settings$startValue else (lower + upper) / 2
+
+  if(nPar > 1) optresul <- optim(par=startVal, fn = target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
+  else optresul <- optim(par=startVal, fn = target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = lower, upper = upper)
+  optresul
+
+}
+
+function preOptimizeMetropolis(bayesianSetup) {
+  target <- function(x) {
+    out <- bayesianSetup$posterior$density(x)
+    if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
+    return(out)
+  }
+  nPar = bayesianSetup$numPars
+
+  lower = if (!is.null(bayesianSetup$prior$lower)) bayesianSetup$prior$lower else rep(-1e20, nPar)
+  upper = if (!is.null(bayesianSetup$prior$upper)) bayesianSetup$prior$upper else rep(+1e20, nPar)
+  # startVal = if (!is.null(settings$startValue)) settings$startValue else (lower + upper) / 2
+  startVal = bayesianSetup$prior$sampler()
+
+  if(nPar > 1) optResul <- optim(par=startVal, fn = target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
+  else optResul <- optim(par=startVal, fn = target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = lower, upper = upper)
+  
+  optPar = optResul$par
+  covMat = Matrix::nearPD(-MASS::ginv(numDeriv::hessian(target, preOpt$par)))$mat
+  list(par = optPar, cov = covMat)
+}
 
 
 
-
-
-
-
-# if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
-# else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)
-# 
-# 
-# 
-# if(is.null(settings$message) || settings$message == TRUE){
-#   cat("BT runMCMC: trying to find optimal start and covariance values", "\b")
-# }
-# 
-# target <- function(x){
-#   out <- bayesianSetup$posterior$density(x)
-#   if (out == -Inf) out =  -1e20 # rnorm(1, mean = -1e20, sd = 1e-20)
-#   return(out)
-# }
-# 
-# try( {
-#   if(bayesianSetup$numPars > 1) optresul <- optim(par=settings$startValue,fn=target, method="Nelder-Mead", hessian=F, control=list("fnscale"=-1, "maxit" = 10000))
-#   else optresul <- optim(par=settings$startValue,fn=target, method="Brent", hessian=F, control=list("fnscale"=-1, "maxit" = 10000), lower = bayesianSetup$prior$lower, upper = bayesianSetup$prior$upper)
-#   settings$startValue = optresul$par
-#   hessian = numDeriv::hessian(target, optresul$par)
-# 
-# 
-#   proposalGenerator$covariance = as.matrix(Matrix::nearPD(MASS::ginv(-hessian))$mat)
-#   #proposalGenerator$covariance = MASS::ginv(-optresul$hessian)
-# 
-#   # Create objects for startValues and covariance to add space between values
-#   startV <-covV <- character()
-# 
-#   for(i in 1:length(settings$startValue)){
-#     startV[i] <- paste(settings$startValue[i], "")
-#   }
-#   for(i in 1:length( proposalGenerator$covariance)){
-#     covV[i] <- paste( proposalGenerator$covariance[i], "")
-#   }
-# 
-#   if(is.null(settings$message) || settings$message == TRUE){
-#     message("BT runMCMC: Optimization finished, setting startValues to " ,
-#             startV, " - Setting covariance to " , covV)
-#   }
-# 
-#   proposalGenerator = updateProposalGenerator(proposalGenerator)
-# 
-# }
-# , silent = FALSE)
-# 
