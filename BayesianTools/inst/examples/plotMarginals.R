15969f39185fac169adf5462aed722b310ef4b10
Florian Hartig <florian.hartig@biologie.uni-regensburg.de>
2018-06-07 23:59:08 +0200

    Extended help for marginanPlot

--- BayesianTools/inst/examples/plotMarginals.R
+++ BayesianTools/inst/examples/plotMarginals.R
@@ -1,2 +1,22 @@
-dat = generateTestDensityMultiNormal(n = 100000, sample = TRUE)
-marginalPlot(dat(10000))
+## Generate a test likelihood function. 
+ll <- generateTestDensityMultiNormal(sigma = "no correlation")
+
+## Create a BayesianSetup object from the likelihood 
+## is the recommended way of using the runMCMC() function.
+bayesianSetup <- createBayesianSetup(likelihood = ll, lower = rep(-10, 3), upper = rep(10, 3))
+
+## Finally we can run the sampler and have a look
+settings = list(iterations = 1000, adapt = FALSE)
+out <- runMCMC(bayesianSetup = bayesianSetup, sampler = "Metropolis", settings = settings)
+
+
+marginalPlot(out)
+marginalPlot(out, plotPrior = F)
+marginalPlot(out, histogram = T)
+marginalPlot(out, singlePanel = T, scale=T, col = c("red", "blue"))
+
+
+
+
+
+
