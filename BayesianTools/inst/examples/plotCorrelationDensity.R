f1d133cfefdf4f9845f6100694aa0bb58984ad6c
Florian Hartig <florian.hartig@biologie.uni-regensburg.de>
2018-06-08 13:05:36 +0200

    Another Help T / TRUE problem

--- BayesianTools/inst/examples/plotCorrelationDensity.R
+++ BayesianTools/inst/examples/plotCorrelationDensity.R
@@ -1,3 +1,3 @@
 dat = generateTestDensityMultiNormal(sigma = "no correlation", sample = TRUE)
 correlationPlot(dat(100000))
-correlationPlot(dat(100000), scaleCorText = F)
+correlationPlot(dat(100000), scaleCorText = FALSE)
