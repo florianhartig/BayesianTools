2bb160c4261eeeaeb5d92453d02d144f2c5a7c47
Florian Hartig <florian@pc1021700460.uni-regensburg.de>
2018-11-19 18:52:21 +0100

    adjustements to logSumExp
    to catch -Inf values in the calculations

---
+++ BayesianTools/tests/testthat/test-utils-Numerics.R
@@ -0,0 +1,22 @@
+context("Utils Numerics work")
+
+skip_on_cran()
+
+test_that("logSumExp", {
+  
+  x = rep(log(1),10)
+  expect_equal(exp(BayesianTools:::logSumExp(x)), 10)
+  expect_equal(exp(BayesianTools:::logSumExp(x,mean = T)), 1)  
+  
+  x = c(rep(log(1),9), -Inf)  
+  expect_equal(exp(BayesianTools:::logSumExp(x)), 9)
+  expect_equal(exp(BayesianTools:::logSumExp(x, mean = T)), 9/10)
+
+  x = rep(-100, 100) 
+  expect_equal(log(sum(exp(x))),   logSumExp(x))
+
+  x = rep(-10000, 100) 
+  expect(logSumExp(x))
+  
+})
+
