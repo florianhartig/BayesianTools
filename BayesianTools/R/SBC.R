b7fab4da81f0a2cd66cb4ea3ca32c56c5fd4d090
Florian Hartig <florian@pc1021700460.uni-regensburg.de>
2018-12-12 16:11:30 +0100

    Create SBC.R

---
+++ BayesianTools/R/SBC.R
@@ -0,0 +1,29 @@
+
+
+#' Simulation-based calibration tests
+#' 
+#' This function data averaged posterior
+#' 
+#' @param mcmcList a list (not a coda::mcmcList) of MCMC results. List items must be of a class supported by getSamples
+#' @param cdfs 
+#' @param referenceSample 
+#' @param type the type of test
+#'
+#' @details 
+#' 
+#' 
+#' data averaged posterior
+#' 
+#'
+#' @export
+#'
+#' Talts, Sean, Michael Betancourt, Daniel Simpson, Aki Vehtari, and Andrew Gelman. "Validating Bayesian Inference Algorithms with Simulation-Based Calibration." arXiv preprint arXiv:1804.06788 (2018).
+#' 
+#' 
+calibrationTest <- function(mcmcList, cdfs = NULL, referenceSample = NULL, type = "SBC"){
+  
+  
+  for(i in 1:)
+  
+  
+}
