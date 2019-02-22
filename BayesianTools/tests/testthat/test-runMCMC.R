fe44f9fa796bd0b9398201f03dc2f2c8278cd182
florianhartig <florian@pc1021700460.uni-regensburg.de>
2019-02-22 15:44:17 +0100

    error catching for gelmanDiagnostics
    fixes #166

--- BayesianTools/tests/testthat/test-runMCMC.R
+++ BayesianTools/tests/testthat/test-runMCMC.R
@@ -6,10 +6,21 @@ library(testthat)
 
 
 ll <- function(x) sum(dnorm(x, log = T))
-bayesianSetup <- createBayesianSetup(likelihood = ll, lower = rep(-10, 3), upper = rep(10, 3))
+bayesianSetup <- createBayesianSetup(likelihood = ll, lower = rep(-10, 4), upper = rep(10, 4))
 
-settings = list(iterations = 200)
+settings = list(iterations = 5)
 out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+
+x = gelmanDiagnostics(out)
+summary(out)
+
+
+settings = list(iterations = 10, nrChains = 3)
+out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+gelmanDiagnostics(out)
+summary(out)
+
+
 expect_error(runMCMC(bayesianSetup = ll, settings = settings))
 
 
