4c2ba298b94480e9b28426586ca6c3c2d1191cc6
florianhartig <florianhartig@users.noreply.github.com>
2021-04-19 20:12:05 +0200

    help updates

--- BayesianTools/R/codaFunctions.R
+++ BayesianTools/R/codaFunctions.R
@@ -3,6 +3,9 @@
 #' @param x a list of MCMC chains
 #' @param merge logical determines whether chains should be merged
 #' @return combined chains
+#' 
+#' @note to combine several chains to a single McmcSamplerList, see \code{\link{createMcmcSamplerList}}
+#' 
 #' @keywords internal
 combineChains <- function(x, merge = T){
   
