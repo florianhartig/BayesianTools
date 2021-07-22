59a79afcaf209e43e76418aea62624c84c1d51cf
Florian Hartig <florianhartig@users.noreply.github.com>
2021-07-22 16:19:14 +0200

    added scaling option to createPriorDensity

--- BayesianTools/inst/examples/createPriorDensity.R
+++ BayesianTools/inst/examples/createPriorDensity.R
@@ -15,9 +15,24 @@ newPrior = createPriorDensity(out, method = "multivariate",
 bayesianSetup <- createBayesianSetup(likelihood = ll, prior = newPrior)
 
 \dontrun{
+  # first prior / posterior
+  marginalPlot(out)
+  
+  # second prior / posterior
   settings = list(iterations = 1000)
-  out <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+  out2 <- runMCMC(bayesianSetup = bayesianSetup, settings = settings)
+  marginalPlot(out2)
+  
+  # change scaling of the prior with the scaling option
+  newPrior = createPriorDensity(out, method = "multivariate",
+                                eps = 1e-10, lower = rep(-10, 3),
+                                upper =  rep(10, 3), best = NULL, scaling = 0.5)
+  
+  apply(newPrior$sampler(1000),2,sd)
 }
+
+
+
 
 
 
