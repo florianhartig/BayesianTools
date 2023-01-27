3d1516acd4e1b5208c38d5c158df1ee73c72b96d
Florian Hartig <florianhartig@users.noreply.github.com>
2023-01-27 18:15:42 +0100

    small updates

--- BayesianTools/inst/examples/DEfamilyHelp.R
+++ BayesianTools/inst/examples/DEfamilyHelp.R
@@ -1,27 +1,33 @@
 library(BayesianTools)
 
 ll <- generateTestDensityMultiNormal(sigma = "no correlation")
-bayesianSetup <- createBayesianSetup(likelihood = ll, lower = rep(-10, 3), upper = rep(10, 3))
+bayesianSetup <- createBayesianSetup(likelihood = ll, 
+                                     lower = rep(-10, 3), 
+                                     upper = rep(10, 3))
 
 settings = list(iterations = 200)
 out <- runMCMC(bayesianSetup = bayesianSetup, sampler = "DEzs", settings = settings)
 summary(out)
 
-# all DE family samplers have a number of internal chains that can be
-# parallelized. Here examples how to change the internal chains
+# DE family samplers are population MCMCs that run a number of internal chains
+# in parallel. Here examples how to change the internal chains
+# note that internal chains can be executedi n parallel
 settings = list(startValue = 4, iterations = 200)
 out <- runMCMC(bayesianSetup = bayesianSetup, sampler = "DEzs", settings = settings)
 summary(out)
 
-# Start with explicit start values
+# Modify the start values of the internal chains (note that this is a matrix
+# of dim nChain * nPar)
 settings = list(startValue = matrix(rnorm(12), nrow = 4, ncol = 3), 
                 iterations = 200)
 out <- runMCMC(bayesianSetup = bayesianSetup, sampler = "DEzs", settings = settings)
 summary(out)
 
-# Start with explicit start values for the Z matrix - this is often useful
+# In the DE sampler family with Z matrix, the previous chains are written in 
+# a common matrix, from which proposals are generated. Per default this matrix
+# is started with samples from the prior, but we can change this. Often useful
 # to improve sampler convergence, 
-# see also https://github.com/florianhartig/BayesianTools/issues/79
+# see  https://github.com/florianhartig/BayesianTools/issues/79
 settings = list(startValue = matrix(rnorm(12), nrow = 4, ncol = 3),
                 Z = matrix(rnorm(300), nrow = 100, ncol = 3),
                 iterations = 200)
