d3479a73ca5f575548688e54a75601b155650734
Florian Hartig <florianhartig@users.noreply.github.com>
2026-03-09 19:24:52 +0100

    rcpp update

--- BayesianTools/src/RcppExports.cpp
+++ BayesianTools/src/RcppExports.cpp
@@ -22,3 +22,13 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_BayesianTools_vsemC", (DL_FUNC) &_BayesianTools_vsemC, 2},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_BayesianTools(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
